969cf94814aeecd3b69383e01534cd692917cd63
Stefan Karschti <stefan.karschti@gmail.com>
2018-10-14 22:11:23 +0300

    .

--- src/kernel/ihandler.c
+++ src/kernel/ihandler.c
@@ -11,88 +11,104 @@ void set_irq_hook(int number, void (*hook)())
 	}
 }
 
+// IRQ0: Timer 
 void irq0_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[0]) irq_hook[0]();
 }
  
+// IRQ1: Keyboard 
 void irq1_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[1]) irq_hook[1]();
 }
  
+// IRQ2: cascade
 void irq2_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[2]) irq_hook[2]();
 }
  
+// IRQ3: COM2 / COM4
 void irq3_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[3]) irq_hook[3]();
 }
  
+// IRQ4: COM1 / COM3
 void irq4_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[4]) irq_hook[4]();
 }
  
+// IRQ5: LPT2 / LPT3
 void irq5_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[5]) irq_hook[5]();
 }
  
+// IRQ6: Floppy 
 void irq6_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[6]) irq_hook[6]();
 }
  
+// IRQ7: LPT1
 void irq7_handler(void) {
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[7]) irq_hook[7]();
 }
  
+// IRQ8: Real Time Clock
 void irq8_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI          
 	if(irq_hook[8]) irq_hook[8]();
 }
  
+// IRQ9: ACPI / free
 void irq9_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[9]) irq_hook[9]();
 }
  
+// IRQ10: free
 void irq10_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[10]) irq_hook[10]();
 }
  
+// IRQ11: free
 void irq11_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[11]) irq_hook[11]();
 }
  
+// IRQ12: PS2 mouse
 void irq12_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[12]) irq_hook[12]();
 }
  
+// IRQ13: IPI / Coprocessor
 void irq13_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[13]) irq_hook[13]();
 }
  
+// IRQ14: ATA1
 void irq14_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI
 	if(irq_hook[14]) irq_hook[14]();
 }
  
+// IRQ15: ATA2
 void irq15_handler(void) {
 	outb(0xA0, 0x20);
 	outb(0x20, 0x20); //EOI
---
+++ src/kernel/keyboard.h
@@ -0,0 +1,12 @@
+#ifndef _KEYBOARD_H_
+#define _KEYBOARD_H_
+
+/** Keyboard driver
+**/
+
+class Keyboard
+{
+};
+
+#endif
+
