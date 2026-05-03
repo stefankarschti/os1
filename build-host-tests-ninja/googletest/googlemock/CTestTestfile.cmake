884f1e91e8291c30cd204bd78c1d1dce9cde0763
Stefan Karschti <4198736+stefankarschti@users.noreply.github.com>
2026-05-03 22:16:09 +0300

    remove build dir

--- build-host-tests-ninja/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/stefan/os1_repo/third_party/googletest/googlemock
-# Build directory: /Users/stefan/os1_repo/build-host-tests-ninja/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
